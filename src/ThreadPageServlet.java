e9a8f84cc8975620f265eb3ef009d029e5ab6f5e
takahashi tooi <tt21304016@ga.ttc.ac.jp>
2022-02-08 16:34:46 +0900

    色々変えた

--- src/ThreadPageServlet.java
+++ src/ThreadPageServlet.java
@@ -1,5 +1,29 @@
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 public class ThreadPageServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
 
+
+		req.setCharacterEncoding("UTF-8");
+		String sql=" insert into cloudy_reply (reply_text, reply_tag) valuse(?,?)";
+
+		String text = req.getParameter("reply_text");
+		String tag = req.getParameter("reply_tag");
+
+		req.setAttribute("text",text);
+		req.setAttribute("tag",tag);
+
+		RequestDispatcher dispatcher=
+				req.getRequestDispatcher("threadpage");
+
+		dispatcher.forward(req, res);
+	}
 }
---
+++ src/sql/ThreadInsertSql.java
@@ -0,0 +1,57 @@
+package sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import bean.textBean;
+
+public class ThreadInsertSql{
+
+	public void setText(textBean bean,String sql){
+		Connection con = null;
+	    PreparedStatement ps = null;
+
+
+    try{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		//Oracleに接続する
+			con=
+			DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl",
+			"info","pro");
+		System.out.println("接続完了");
+
+		//select文
+		String sql=" insert into cloudy_reply (reply_text, reply_tag) valuse(?,?)";
+
+		ps.setString(1,bean.getText());
+		ps.setString(2,bean.getText());
+
+
+
+		//Statementインターフェイスを実装するクラスをインスタンス化する
+		ps = con.prepareStatement(sql);
+
+		ps.executeUpdate();
+
+
+
+
+
+		//Oracleから切断する
+		con.close();
+
+		System.out.println("切断完了");
+	}catch(ClassNotFoundException e){
+		e.printStackTrace();
+		System.out.println("クラスがないみたい。");
+	}catch(SQLException e){
+		e.printStackTrace();
+		System.out.println("SQL関連の例外みたい。");
+	}catch(Exception e){
+		e.printStackTrace();
+	}
+}
+}
