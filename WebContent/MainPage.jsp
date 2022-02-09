531ff848e93587f2227958099f5db25b5eea51a3
yuuga <yh21304026@ga.ttc.ac.jp>
2022-02-09 14:04:59 +0900

    no message

--- WebContent/MainPage.jsp
+++ WebContent/MainPage.jsp
@@ -7,37 +7,15 @@
 <meta charset="UTF-8">
 
 
-<script type="text/javascript">
-
-function check(){
-    var flag = 0;
-
-    if(document.form1.name.value == ""){
-        flag = 1;
-    }
-    else if(document.form1.comment.value == ""){
-        flag = 1;
-    }
-
-    if(flag){
-        window.alert('名前とコメントを入力してください');
-        return false; // 送信を中止
-    }
-    else{
-        return true; // 送信を実行
-    }
-}
-</script>
-
 <title>掲示板</title>
 </head>
 <body>
 <form action="/board/BoardServlet" method="post" name="form1" onSubmit="return check()">
-<p>名前:<input type="text" name="name"></p>
+
 <p>コメント:<br>
 <textarea name="comment" rows="5" cols="40"></textarea>
 </p>
-<p><input type="submit" value="送信"><input type="reset" value="リセット">
+<p><input type="submit" value="TWEET">
 </p>
 </form>
 
--- src/MainPageServlet.java
+++ src/MainPageServlet.java
@@ -1,84 +1,40 @@
+
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import main.AddCommentLogic;
-import main.Board;
-import main.FindCommentLogic;
 
-/**
- * Servlet implementation class BoardServlet
- */
-@WebServlet("/BoardServlet")
+@WebServlet("/tweet")
 public class MainPageServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+	List<String> tweetlist = new ArrayList<String>();
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MainPageServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
 
-        //リスナークラスに移動したい
-        request.setCharacterEncoding("UTF-8");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String tweet = request.getParameter("tweet");
+		tweetlist.add(0,tweet);
 
-        // 既存のコメントを確認
-        FindCommentLogic fcl = new FindCommentLogic();
-        List<Board> list = fcl.executeFindComment();
+		ServletContext application = this.getServletContext();
+		application.setAttribute("tweet", tweetlist);
 
-        // セッションスコープにコメントリストを保存
-        HttpSession session = request.getSession();
-        session.setAttribute("listAttribute", list);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
 
-        RequestDispatcher rd =request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
-        rd.forward(request, response);
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        request.setCharacterEncoding("UTF-8");
-
-        // 入力された値を取得
-        String name = request.getParameter("name");
-        String comment = request.getParameter("comment");
-
-        //JavaBeansに格納
-        Board bo = new Board();
-        bo.setName(name);
-        bo.setComment(comment);
-
-        // mysqlに格納
-        AddCommentLogic acl = new AddCommentLogic();
-        acl.executeAddComment(bo);
-
-        // 今入力されたコメントと既存のコメントをmysqlから取得
-        FindCommentLogic fcl = new FindCommentLogic();
-        List<Board> list = fcl.executeFindComment();
-
-        // セッションスコープにコメントリストを保存
-        HttpSession session = request.getSession();
-        session.setAttribute("listAttribute", list);
-
-        RequestDispatcher rd =request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
-        rd.forward(request, response);
-
-    }
+	}
 
 }
